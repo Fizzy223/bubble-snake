6422dee04b36981609dc0bec91993bd853ed66fc
Fizzy <goraboraoraora@gmail.com>
2026-07-18 00:18:16 +0630

    Create Snake and basic movement function

---
+++ app/components/Game.tsx
@@ -0,0 +1,100 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "../styles/colors";
+import { Coordinate, Direction } from "../types/types";
+import Snake from "./Snake";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
+export default function Game() {
+  const [direction, setDirection] = React.useState<Direction>(Direction.Right);
+  const [snake, setSnake] = React.useState<Coordinate[]>(
+    SNAKE_INITIAL_POSITION,
+  );
+  const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION);
+  const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+  const [isPaused, setIsPaused] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (!isGameOver) {
+      const intervalId = setInterval(() => {
+        !isPaused && moveSnake();
+      }, MOVE_INTERVAL);
+      return () => clearInterval(intervalId);
+    }
+  }, [snake, isGameOver, isPaused]);
+
+  const moveSnake = () => {
+    const snakeHead = snake[0];
+    const newHead = { ...snakeHead };
+    switch (direction) {
+      case Direction.Up:
+        newHead.y -= 1;
+        break;
+      case Direction.Down:
+        newHead.y += 1;
+        break;
+      case Direction.Left:
+        newHead.x -= 1;
+        break;
+      case Direction.Right:
+        newHead.x += 1;
+        break;
+      default:
+        break;
+    }
+
+    setSnake([newHead, ...snake].slice(0, -1));
+  };
+
+  const handleGesture = Gesture.Pan()
+    .runOnJS(true)
+    .onUpdate((event) => {
+      const { translationX, translationY } = event;
+
+      if (Math.abs(translationX) > Math.abs(translationY)) {
+        if (translationX > 0) {
+          setDirection(Direction.Right);
+        } else {
+          setDirection(Direction.Left);
+        }
+      } else {
+        if (translationY > 0) {
+          setDirection(Direction.Down);
+        } else {
+          setDirection(Direction.Up);
+        }
+      }
+    });
+
+  return (
+    <GestureDetector gesture={handleGesture}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.boundaries}>
+          <Snake snake={snake} />
+        </View>
+      </SafeAreaView>
+    </GestureDetector>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+  },
+  boundaries: {
+    flex: 1,
+    borderColor: Colors.primary,
+    borderWidth: 12,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    backgroundColor: Colors.background,
+  },
+});
